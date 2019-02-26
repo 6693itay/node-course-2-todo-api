a8184f1ae1ca209725f4cfb9504362b5726ad3fa
Itay <6693itay@gmail.com>
2019-02-26 16:54:01 +0200

    a

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(false||'mongodb://Itay:1212@cluster0-shard-00-00-xlmna.mongodb.net:27017,cluster0-shard-00-01-xlmna.mongodb.net:27017,cluster0-shard-00-02-xlmna.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true')
+mongoose.connect(false||'mongodb://3.80.204.187:27017/TodoApp')
 module.exports={mongoose};
